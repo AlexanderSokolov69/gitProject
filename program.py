ce4fe9a0822aa3562a466207b0ca544c702dac29
AlexanderSokolov69 <70482091+AlexanderSokolov69@users.noreply.github.com>
2021-11-19 16:49:03 +0300

    gitHub edit

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program!')
     print('My Second Chalenge!')
     print('333333333333333333333333')
+    print ('git edit try')
     
 if __name__ == '__main__':
     main()
